aaa94447e1a2fdc5c120198177e3d0f740628559
Carlos <147120177+carlos11henrique@users.noreply.github.com>
2024-10-13 20:39:14 -0300

    Update app.js
    cadrasto dos Usuarios

--- src/app.js
+++ src/app.js
@@ -9,14 +9,21 @@ const chamadosRoutes = require('./routes/chamadosRoutes');
 const chamadosMaquinasRoutes = require('./routes/chamados_maquinasRoutes');
 const atribuídosRoutes = require('./routes/atribuidosRoutes');
 const logsRoutes = require('./routes/logsRoutes');
+const cors = require('cors');
+const usuarioModel = require('./models/usuariosModel');
+const usuariosController = require('./controllers/usuariosController');
+
 
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Bem-vindo à API! Utilize as rotas disponíveis para acessar os recursos.');
 });
+app.post('/login', (req, res) => {
 
+});
+app.post('/register', usuariosController.create);
 app.use('/setores', setoresRoutes);
 app.use('/usuarios', usuariosRoutes);
 app.use('/blocos', blocosRoutes);
